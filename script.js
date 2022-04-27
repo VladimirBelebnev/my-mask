73e45757353af0984f4dba896504a405a860df2a
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-04-27 18:23:21 +0300

    add

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import phoneMask from './phoneMask';
 import textMask from './textMask';
+import onlyNumber from './onlyNumber';
 
 phoneMask('[name="phone"]');
 textMask('[name="text"]');
+onlyNumber('[name="phone"]');
