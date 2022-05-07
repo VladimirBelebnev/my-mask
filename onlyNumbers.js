cbd629ec0236fadede648fbd29d4e5ddf2565181
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-05-07 11:38:29 +0300

    Update onlyNumbers.js

--- onlyNumbers.js
+++ onlyNumbers.js
@@ -1,11 +1,11 @@
 const onlyNumbers = () => {
-  const number = document.querySelector(selector);
+  const number = document.querySelectorAll(selector);
 
   number.forEach(item => {
     item.addEventListener('input', () => {
       item.value = item.value.replace(/\D/, '');
     });
   });
-}
+};
 
 export default onlyNumbers;
